bb6532a96335ff9c3834c05e42ab3af8c7712f1a
dallintine <dallintine@gmail.com>
2020-04-08 10:57:36 +0100

    more task

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,25 @@
 const covid19ImpactEstimator = (data) => {
 const input = data;
-    return {
-        data: {},
+    return { data, impact, severeImpact };
+};
+
+const covid19ImpactEstimator = (data) => {
+    const estimator = chain(
+        estiomateCurrentlyInfected,
+        estimatePrjectedInfections,
+
+        estmateServereCases,
+        estimateBedSpeceAvailability,
+
+        estimateCasesForICU,
+        estmateCasesForVentilotors,
+        estimateDollarsInFlight
+    );
+
+    return estimator({
+        data,
         impact: {},
         severeImpact: {}
-    };
+    });
 };
 export default covid19ImpactEstimator;
