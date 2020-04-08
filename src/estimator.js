40c674c9e5caec60eae20e580f12ffb195add40c
dallintine <dallintine@gmail.com>
2020-04-09 00:48:03 +0100

    changes new

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,14 @@
+
+
+
 const covid19ImpactEstimator = (data) => {
 const input = data;
+impact.currentlyInfected=reportedCases * 10;
+impact=currentlyInfected * 512;
+severeImpact.currentlyInfected = reportedCases * 50;
+severeImpact=currentlyInfected * 512;
 return { data, impact, severeImpact };
+console.log(covid19ImpactEstimator);
 };
 
 const covid19ImpactEstimators = (data) => {
