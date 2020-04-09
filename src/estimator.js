5f406ba2baacc874c46a3e304015d7d974f50206
dallintine <dallintine@gmail.com>
2020-04-09 01:14:11 +0100

    changes1

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,29 @@
 
 
 
-const covid19ImpactEstimator = (data) => {
+const covid19ImpactEstimator = (data) => data; 
+{
 const input = data;
-impact.currentlyInfected=reportedCases * 10;
-impact=currentlyInfected * 512;
-severeImpact.currentlyInfected = reportedCases * 50;
-severeImpact=currentlyInfected * 512;
-return { data, impact, severeImpact };
-console.log(covid19ImpactEstimator);
+return{
+    data: input,
+    impact: {
+        currentlyInfected: reportedCases * 10,
+        infectionsByRequestedTime: currentlyInfected * 1024
+    },
+    severeImpact: {
+        currentlyInfected: reportedCases * 50,
+        infectionsByRequestedTime: currentlyInfected * 1024
+    }
 };
+// impact.currentlyInfected=reportedCases * 10;
+// impact=currentlyInfected * 512;
+// severeImpact.currentlyInfected = reportedCases * 50;
+// severeImpact=currentlyInfected * 512;
+// return { data, impact, severeImpact };
+// console.log(data);
+// };
 
-const covid19ImpactEstimators = (data) => {
+// const covid19ImpactEstimators = (data) => {
 const estimator = chain(
         estiomateCurrentlyInfected,
         estimatePrjectedInfections,
