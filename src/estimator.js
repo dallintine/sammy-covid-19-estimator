c8893e7610ad566fcb26b7b45880325f6e9b6f37
dallintine <dallintine@gmail.com>
2020-04-09 12:32:10 +0100

    new changed

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,34 @@
+const covid19ImpactEstimator = (data) => data;
+export const impact = (data) => {
+    const{
+        periodType,
+        timeToElapse,
+        reportedCases,
+        totalHospitalBeds,
+        region
+    } = data;
+    const currentlyInfected = reportedCases * 10;
+    const timeInDays = convertToDays(periodType, timeToElapse);
+    const infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor(timeInDays / 3));
+    const severCasesByRequestedTime = Math.floor(totalHospitalBeds * 0.35);
+    const hospitalBedsAvailable = Math.floor(totalHospitalBeds * 0.35);
+    const hospitalBedsByResquestedTime = hospitalBedsAvailable - severCasesByRequestedTime;
+    const casesForICUByRequestedTime = Math.floor(infectionsByRequestedTime * 0.05);
+    const casesForVentilatorByRequestedTime = Math.floor(infectionsByRequestedTime * 0.02);
+    const dollerOut = region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD * timeInDays;
+    const toTwoDecimal = dollerOut.toFixed(2);
+    const dollersInFlight = Number(toTwoDecimal);
+
+    return {
+        currentlyInfected,
+        infectionsByRequestedTime,
+        severCasesByRequestedTime,
+        hospitalBedsByResquestedTime,
+        casesForICUByRequestedTime,
+        casesForVentilatorByRequestedTime,
+        dollersInFlight
+    };
+};
 
 export const severeImpactCases = (data) =>{
     const {
