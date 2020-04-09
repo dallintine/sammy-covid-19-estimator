69fa22f46e465b49c33055841cfed5de86eb404e
dallintine <dallintine@gmail.com>
2020-04-09 02:05:44 +0100

    newfile

--- src/estimator.js
+++ src/estimator.js
@@ -1,46 +1,41 @@
 
+export const severeImpactCases = (data) =>{
+    const {
+        periodType,
+        timeToElapse,
+        reportedCases,
+        totalHospitalBeds,
+        region
+    } = data;
+    const currentlyInfected = reportedCases * 50;
+    const timeInDays = convertToDays(periodType, timeToElapse);
+    const infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor(timeInDays / 3));
+    const severCasesByRequestedTime = Math.floor(totalHospitalBeds * 0.35);
+    const hospitalBedsAvailable = Math.floor(totalHospitalBeds * 0.35);
+    const hospitalBedsByResquestedTime = hospitalBedsAvailable - severCasesByRequestedTime;
+    const casesForICUByRequestedTime = Math.floor(infectionsByRequestedTime * 0.05);
+    const casesForVentilatorByRequestedTime = Math.floor(infectionsByRequestedTime * 0.02);
+    const dollerOut = region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD * timeInDays;
+    const toTwoDecimal = dollerOut.toFixed(2);
+    const dollersInFlight = Number(toTwoDecimal);
 
-
+    return {
+        currentlyInfected,
+        infectionsByRequestedTime,
+        severCasesByRequestedTime,
+        hospitalBedsByResquestedTime,
+        casesForICUByRequestedTime,
+        casesForVentilatorByRequestedTime,
+        dollersInFlight
+    };
+};
 const covid19ImpactEstimator = (data) =>  
 {
-const input = data;
-return{
-    data: input,
-    impact: {
-        currentlyInfected: reportedCases * 10,
-        infectionsByRequestedTime: currentlyInfected * 1024
-    },
-    severeImpact: {
-        currentlyInfected: reportedCases * 50,
-        infectionsByRequestedTime: currentlyInfected * 1024
-    }
-};
-// impact.currentlyInfected=reportedCases * 10;
-// impact=currentlyInfected * 512;
-// severeImpact.currentlyInfected = reportedCases * 50;
-// severeImpact=currentlyInfected * 512;
-// return { data, impact, severeImpact };
-// console.log(data);
-// };
-
-// const covid19ImpactEstimators = (data) => {
-// const estimator = chain(
-//         estiomateCurrentlyInfected,
-//         estimatePrjectedInfections,
-    
-//         estmateServereCases,
-//         estimateBedSpeceAvailability,
-
-//         estimateCasesForICU,
-//         estmateCasesForVentilotors,
-//         estimateDollarsInFlight
-//     );
-//     return estimator({
-//         data,
-//         impact: {},
-//         severeImpact: {}
-//     });
-    
+    {
+    data,
+    impact: impactCases(data),
+    severeImpact: severeImpactCases(data)
+   }
 };
 
 export default covid19ImpactEstimator;
