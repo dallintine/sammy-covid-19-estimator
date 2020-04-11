fd82c2eabc5341a484aeacfa3cac3a6784bdd8a5
dallintine <dallintine@gmail.com>
2020-04-11 12:25:33 +0100

    new neooo

--- src/estimator.js
+++ src/estimator.js
@@ -1,70 +1,69 @@
 const covid19ImpactEstimator = (data) => {
-    {
-        data,
-        impact; impactCases(data),
-        severeImpact; severeImpactCases(data)
-    }
+  {
+    data,
+    impact; impactCases(data),
+    severeImpact; severeImpactCases(data)
+  };
 };
 
 export const impactCases = (data) => {
-    const {
-        periodType,
-        timeToElapse,
-        reportedCases,
-        totalHospitalBeds,
-        region
-    } = data;
-    const currentlyInfected = reportedCases * 10;
-    const timeInDays = convertToDays(periodType, timeToElapse);
-    const infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor(timeInDays / 3));
-    const severCasesByRequestedTime = Math.trunc(totalHospitalBeds * 0.35);
-    const hospitalBedsAvailable = Math.trunc(totalHospitalBeds * 0.35);
-    const hospitalBedsByResquestedTime = hospitalBedsAvailable - severCasesByRequestedTime;
-    const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
-    const casesForVentilatorByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
-    const dollerOut = region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD * timeInDays;
-    const toTwoDecimal = dollerOut.toFixed(2);
-    const dollersInFlight = Number(toTwoDecimal);
-    
-    return {
-        currentlyInfected,
-        infectionsByRequestedTime,
-        severCasesByRequestedTime,
-        hospitalBedsByResquestedTime,
-        casesForICUByRequestedTime,
-        casesForVentilatorByRequestedTime,
-        dollersInFlight
-    };
-};
+  const {
+    periodType,
+    timeToElapse,
+    reportedCases,
+    totalHospitalBeds,
+    region
+  } = data;
+  const currentlyInfected = reportedCases * 10;
+  const timeInDays = convertToDays(periodType, timeToElapse);
+  const infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor(timeInDays / 3));
+  const severCasesByRequestedTime = Math.trunc(totalHospitalBeds * 0.35);
+  const hospitalBedsAvailable = Math.trunc(totalHospitalBeds * 0.35);
+  const hospitalBedsByResquestedTime = hospitalBedsAvailable - severCasesByRequestedTime;
+  const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
+  const casesForVentilatorByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
+  const dollerOut = region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD * timeInDays;
+  const toTwoDecimal = dollerOut.toFixed(2);
+  const dollersInFlight = Number(toTwoDecimal);
 
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severCasesByRequestedTime,
+    hospitalBedsByResquestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorByRequestedTime,
+    dollersInFlight
+  };
+};
 export const severeImpactCases = (data) => {
-    const {
-        periodType,
-        timeToElapse,
-        reportedCases,
-        totalHospitalBeds,
-        region
-    } = data;
-    const currentlyInfected = reportedCases * 50;
-    const timeInDays = convertToDays(periodType, timeToElapse);
-    const infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor(timeInDays / 3));
-    const severCasesByRequestedTime = Math.trunc(totalHospitalBeds * 0.35);
-    const hospitalBedsAvailable = Math.trunc(totalHospitalBeds * 0.35);
-    const hospitalBedsByResquestedTime = hospitalBedsAvailable - severCasesByRequestedTime;
-    const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
-    const casesForVentilatorByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
-    const dollerOut = region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD * timeInDays;
-    const toTwoDecimal = dollerOut.toFixed(2);
-    const dollersInFlight = Number(toTwoDecimal);
-    
-    return {
-        currentlyInfected,
-        infectionsByRequestedTime,
-        severCasesByRequestedTime,
-        hospitalBedsByResquestedTime,
-        casesForICUByRequestedTime,
-        casesForVentilatorByRequestedTime,
-        dollersInFlight
-    };
+  const {
+    periodType,
+    timeToElapse,
+    reportedCases,
+    totalHospitalBeds,
+    region
+  } = data;
+  const currentlyInfected = reportedCases * 50;
+  const timeInDays = convertToDays(periodType, timeToElapse);
+  const infectionsByRequestedTime = currentlyInfected * (2 ** Math.floor(timeInDays / 3));
+  const severCasesByRequestedTime = Math.trunc(totalHospitalBeds * 0.35);
+  const hospitalBedsAvailable = Math.trunc(totalHospitalBeds * 0.35);
+  const hospitalBedsByResquestedTime = hospitalBedsAvailable - severCasesByRequestedTime;
+  const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
+  const casesForVentilatorByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
+  const dollerOut = region.avgDailyIncomePopulation * region.avgDailyIncomeInUSD * timeInDays;
+  const toTwoDecimal = dollerOut.toFixed(2);
+  const dollersInFlight = Number(toTwoDecimal);
+
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severCasesByRequestedTime,
+    hospitalBedsByResquestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorByRequestedTime,
+    dollersInFlight
+  };
 };
 export default covid19ImpactEstimator;
