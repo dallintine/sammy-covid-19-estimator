120318a0e0cae496e0d870f54657f2d6b68012d7
dallintine <dallintine@gmail.com>
2020-04-13 22:42:08 +0100

    talkmore

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,26 @@
+const convertMonthsToDays = (months) => months * 30;
+
+/**
+ *
+ * @param weeks
+ *
+ * Takes the number of weeks anc converts it to days
+ */
+const convertWeeksToDays = (weeks) => weeks * 7;
+
+/**
+ *
+ * @param periodType
+ * @param timeToElapse
+ *
+ * Takes the periodType and the timeToElapse and returns the days equivalent.
+ */
 const convertToDays = (periodType, timeToElapse) => {
   switch (periodType) {
     case 'months':
-      return timeToElapse * 30;
+      return convertMonthsToDays(timeToElapse);
     case 'weeks':
-      return timeToElapse * 7;
+      return convertWeeksToDays(timeToElapse);
     default:
       return timeToElapse;
   }
